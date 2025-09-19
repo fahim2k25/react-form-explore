6933c5dda0b4ff541b04aca802295d0872c4a2b1
fahim2k25 <fahim.program@gmail.com>
2025-09-19 17:28:16 +0600

    implemented how to target and retrieve input values

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
 import './SimpleForm.css'
 
 const SimpleForm = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Our form submitted");
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className='inputs'>
                 <label name="name">
                     Enter your name:
                 </label>
-                <input name='name' placeholder='N A M E'></input>
+                <input type='text' name='name' placeholder='N A M E'></input>
             </div>
             <div className='inputs'>
                 <label name="email">
                     Enter your email:
                 </label>
-                <input name='name' placeholder='E - M A I L'></input>
+                <input type='text' name='email' placeholder='E - M A I L'></input>
             </div>
+            <input type="submit" value="Submit" />
         </form>
     );
 };
