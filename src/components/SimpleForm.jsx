2fcb3a34611f5bbf389dfa458fdc814703f8abae
fahim2k25 <fahim.program@gmail.com>
2025-09-19 16:12:30 +0600

    added some css flares to the inputs relating to Name

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import './SimpleForm.css'
 
 const SimpleForm = () => {
     return (
         <form>
-            <label name="name">
-                Enter your name:
-            </label>
-            <input name='name' placeholder='N A M E'></input>
+            <div className='inputs'>
+                <label name="name">
+                    Enter your name:
+                </label>
+                <input name='name' placeholder='N A M E'></input>
+            </div>
 
         </form>
     );
