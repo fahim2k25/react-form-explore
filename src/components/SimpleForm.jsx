e9287619be2ec987ac48f93c3d6513a3e6accd09
fahim2k25 <fahim.program@gmail.com>
2025-09-19 16:05:52 +0600

    first commit with initial FORM

---
+++ src/components/SimpleForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SimpleForm = () => {
+    return (
+        <form>
+            <label name="name">
+                Enter your name:
+            </label>
+            <input name='name' placeholder='N A M E'></input>
+
+        </form>
+    );
+};
+
+export default SimpleForm;
