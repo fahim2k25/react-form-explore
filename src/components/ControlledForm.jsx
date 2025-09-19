8f9a9fff05a761068a2362d87b175f8d82bd7276
fahim2k25 <fahim.program@gmail.com>
2025-09-19 19:10:11 +0600

    implemented slight password catch and console logging each keystrokes for the third form

--- src/components/ControlledForm.jsx
+++ src/components/ControlledForm.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 
 const ControlledForm = () => {
+
+    const [pass, setPass] = useState("");
+
+    const handlesSubmit = (e) => {
+        e.preventDefault();
+        console.log("Submit 3")
+    }
+    const handleCahnge = (event) => {
+        console.log(event.target.value)
+    }
+
     return (
-        <form className="inputs">
+        <form className="inputs2" onSubmit={handlesSubmit}>
             <input type="text" name="name" placeholder="N A M E" required />
             <input type="email" name="email" placeholder="E M A I L" required />
-            <input type="password" name="password" placeholder="Pass WORD" />
+            <input type="password" onChange={handleCahnge} defaultValue={pass} name="password" placeholder="Pass WORD" />
             <input type="submit" value="Submit" />
         </form>
     );
