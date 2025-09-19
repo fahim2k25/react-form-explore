d2f5c3cb8274cf4d983d497145adcd37dd2e65bb
fahim2k25 <fahim.program@gmail.com>
2025-09-19 21:43:25 +0600

    added validation in real time for password entry

--- src/components/ControlledForm.jsx
+++ src/components/ControlledForm.jsx
@@ -3,22 +3,34 @@ import { useState } from "react";
 const ControlledForm = () => {
 
     const [pass, setPass] = useState("");
-
+    const [err, setErr] = useState("")
     const handlesSubmit = (e) => {
         e.preventDefault();
         console.log("Submit 3")
     }
     const handleCahnge = (event) => {
-        console.log(event.target.value)
+        setPass(event.target.value);
+        if (pass.length < 8) {
+            setErr("Password must be more than 8 characters")
+        } else {
+            setErr("");
+        }
+
     }
 
     return (
-        <form className="inputs2" onSubmit={handlesSubmit}>
-            <input type="text" name="name" placeholder="N A M E" required />
-            <input type="email" name="email" placeholder="E M A I L" required />
-            <input type="password" onChange={handleCahnge} defaultValue={pass} name="password" placeholder="Pass WORD" />
-            <input type="submit" value="Submit" />
-        </form>
+        <div>
+            <form className="inputs2" onSubmit={handlesSubmit}>
+                <input type="text" name="name" placeholder="N A M E" required />
+                <input type="email" name="email" placeholder="E M A I L" required />
+                <input type="password" onChange={handleCahnge} defaultValue={pass} name="password" placeholder="Pass WORD" />
+                <input type="submit" value="Submit" />
+            </form>
+            <p><small style={{ color: "orange", fontStyle: "italic" }}>
+                {err}
+            </small></p>
+        </div>
+
     );
 };
 
