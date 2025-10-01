d63649d7cddfb4469fda7fe7de1563868c64a04a
fahim2k25 <fahim.program@gmail.com>
2025-10-01 19:53:12 +0600

    created the table for new products

--- src/components/ProductManagement/ProductTable.jsx
+++ src/components/ProductManagement/ProductTable.jsx
@@ -4,7 +4,35 @@ const ProductTable = ({ products }) => {
     // const { name, price, quantity } = products;
     return (
         <div>
-            Total Number Products Availaable : {products.length}
+            <p>
+                Total Number Products Available : {products.length}
+            </p>
+
+            <table>
+                <caption>
+                    Product detail for shopping
+                </caption>
+                <thead>
+                    <td>Product Name</td>
+                    <td>Price</td>
+                    <td>Quantity</td>
+                </thead>
+                <tbody>
+                    {
+
+                        products.map((elem) => {
+                            return (
+                                <tr>
+                                    <td>{elem.name}</td>
+                                    <td>{elem.price}</td>
+                                    <td>{elem.quantity}</td>
+                                </tr>
+
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
